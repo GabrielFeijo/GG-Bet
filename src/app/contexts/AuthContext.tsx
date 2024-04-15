1f5eb2a1d613c114954ab667d6f645cc5e59b76d
Gabriel Feijó <92588394+GabrielFeijo@users.noreply.github.com>
2024-04-14 23:35:26 -0300

    feat: ✨ add context to store and share user information.

---
+++ src/app/contexts/AuthContext.tsx
@@ -0,0 +1,29 @@
+'use client';
+import { ReactNode, createContext, useState } from 'react';
+
+export interface AuthProviderProps {
+	children: ReactNode;
+}
+
+interface User {
+	balance: number;
+}
+
+interface AuthContextData {
+	userInformation: User;
+	setUserInformation: React.Dispatch<React.SetStateAction<User>>;
+}
+
+export const AuthContext = createContext({} as AuthContextData);
+
+export function AuthProvider({ children }: AuthProviderProps) {
+	const [userInformation, setUserInformation] = useState<User>({
+		balance: 1000,
+	});
+
+	return (
+		<AuthContext.Provider value={{ userInformation, setUserInformation }}>
+			{children}
+		</AuthContext.Provider>
+	);
+}
